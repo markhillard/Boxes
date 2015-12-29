d9ebb4fa765ff3862633b42dbfe649f2834c4c77
markhillard <markhillard@gmail.com>
2015-12-29 13:39:47 -0500

    Added boxes.js

---
+++ boxes.js
@@ -0,0 +1,17 @@
+(function () {
+    document.body.innerHTML += "\
+        <style>\
+            * {\
+            background:#000 !important;\
+            border-color:#000 !important;\
+            box-shadow:0 0 1px 0 #a94442 !important;\
+            color:#fff !important;\
+            cursor:default !important;\
+            }\
+            \
+            *:hover {\
+            box-shadow:0 0 1px 0 #3c763d !important;\
+            }\
+        </style>\
+    ";
+})();
